870b4db68cd94a52f3a92f3b8af3493c97137b6a
gan.jin <gan.jin.cn@gmail.com>
2021-05-31 22:33:19 +0800

    build(webpb): prevent push to master by git hooks

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,3 +12,14 @@ dependencies {
     implementation("com.github.ben-manes:gradle-versions-plugin:0.38.0")
     implementation("org.springframework.boot:spring-boot-gradle-plugin:2.5.0")
 }
+
+tasks.create<Copy>("initGitHooks") {
+    val hooksDir = project.file("src/main/resources/hooks")
+    from(hooksDir)
+    into(project.file("../.git/hooks"))
+    fileMode = 755
+}
+
+tasks.build {
+    dependsOn("initGitHooks")
+}
