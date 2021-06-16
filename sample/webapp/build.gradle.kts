4f3ed7668afca6c98be2d73e331b7990cce0de6b
gan.jin <gan.jin.cn@gmail.com>
2021-06-16 17:46:17 +0800

    ci(sample): ci test webapp sample

--- sample/webapp/build.gradle.kts
+++ sample/webapp/build.gradle.kts
@@ -8,8 +8,30 @@ dependencies {
   protobuf(project(":sample:proto"))
 }
 
+task<Exec>("npmInstall") {
+  val nodeModules = file("./node_modules")
+  if (nodeModules.exists()) {
+    commandLine("npm", "--version")
+  } else{
+    commandLine("npm", "install")
+  }
+}
+
 task<Exec>("npmStart") {
   commandLine("npm", "run", "dev")
 
+  dependsOn("npmInstall")
   dependsOn("generateProto")
+}
+
+task<Exec>("npmCheck") {
+  commandLine("npm", "run", "lint")
+  commandLine("npm", "run", "test")
+
+  dependsOn("npmInstall")
+  dependsOn("generateProto")
+}
+
+tasks.check {
+  dependsOn("npmCheck")
 }
